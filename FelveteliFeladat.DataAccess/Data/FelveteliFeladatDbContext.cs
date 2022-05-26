bce6ecdb169553a042aae608d345f404b9a5ed7c
b.gyorfi <b.gyorfi@twinner.com>
2022-05-26 19:37:06 +0200

    Added data

--- FelveteliFeladat.DataAccess/Data/FelveteliFeladatDbContext.cs
+++ FelveteliFeladat.DataAccess/Data/FelveteliFeladatDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FelveteliFeladat.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,43 @@ namespace FelveteliFeladat.DataAccess.Data
 {
     public class FelveteliFeladatDbContext : DbContext
     {
+        public virtual DbSet<Team> MyProperty { get; set; }
+        public FelveteliFeladatDbContext()
+        {
+
+        }
+        public FelveteliFeladatDbContext(DbContextOptions<FelveteliFeladatDbContext> options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseInMemoryDatabase("database")
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Team[] teams = new Team[]
+            {
+            new Team("Mercedes", 1967, 10, true),
+            new Team("Ferrari", 1967, 40, true),
+            new Team("Red Bull", 1967, 8, true),
+            new Team("McLaren", 1967, 3, true),
+            new Team("Alpine", 1967, 3, true),
+            new Team("Haas", 1967, 0, true),
+            new Team("Aston Martin", 1967, 0, true),
+            new Team("Alpha Tauri", 1967, 0, true),
+            new Team("Alfa Romeo", 1967, 1, true),
+            new Team("Williams", 1967, 6, true)
+        };
+
+            modelBuilder.Entity<Team>().HasData(teams);
+        }
     }
 }
--- FelveteliFeladat/Models/Team.cs
+++ FelveteliFeladat/Models/Team.cs
@@ -2,9 +2,19 @@
 {
     public class Team
     {
+        public int Id { get; }
+
         public string Name { get; set; }
         public int FoundationYear { get; set; }
         public int NumberOfChampionshipWins { get; set; }
         public bool IsPayedTheEntryFee { get; set; }
+
+        public Team(string name, int foundationYear, int numberOfChampionshipWins, bool isPayedTheEntryFee)
+        {
+            Name = name;
+            FoundationYear = foundationYear;
+            NumberOfChampionshipWins = numberOfChampionshipWins;
+            IsPayedTheEntryFee = isPayedTheEntryFee;
+        }
     }
 }
