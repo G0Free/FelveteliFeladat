712507de024a29fe4ab26dc98940ac07149dd77e
b.gyorfi <b.gyorfi@twinner.com>
2022-05-26 18:42:37 +0200

    Add project files.

---
+++ FelveteliFeladat/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace FelveteliFeladat.Models
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public int FoundationYear { get; set; }
+        public int NumberOfChampionshipWins { get; set; }
+        public bool IsPayedTheEntryFee { get; set; }
+    }
+}
