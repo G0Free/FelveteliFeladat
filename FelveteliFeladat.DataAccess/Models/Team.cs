5d9322aa63a669113092ba8e6cbd279924424dbb
b.gyorfi <b.gyorfi@twinner.com>
2022-06-03 17:57:26 +0200

    Spacing issue fixed

--- FelveteliFeladat.DataAccess/Models/Team.cs
+++ FelveteliFeladat.DataAccess/Models/Team.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FelveteliFeladat.Models
@@ -9,12 +10,20 @@ namespace FelveteliFeladat.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public override int Id { get; set; }
         
+        [Required]
         [MaxLength(50)]
-        public string Name { get; set; }        
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Foundation Year")]
         public int FoundationYear { get; set; }
 
+        [Required]
         [Range(0,int.MaxValue)]
+        [DisplayName("Number of Championship Wins")]
         public int NumberOfChampionshipWins { get; set; }
+
+        [DisplayName("Entry Fee is payed")]
         public bool IsPayedTheEntryFee { get; set; }
 
         public Team(int id, string name, int foundationYear, int numberOfChampionshipWins, bool isPayedTheEntryFee)
